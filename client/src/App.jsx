3078fa9d99e9eaae156cd8dc38fbd9fe343ec3cc
Karan Singh <devkaran.codes@gmail.com>
2025-10-22 14:14:16 +0530

    tailwind and asset setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <div className="text-orange-400">App</div>;
+};
 
-export default App
+export default App;
