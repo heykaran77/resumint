9803601ddef0303b19d98e81c2d31ea714d7263f
Karan Singh <devkaran.codes@gmail.com>
2025-10-28 13:50:15 +0530

    add create resume functionality

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,13 +1,35 @@
-import { FilePenIcon, PlusIcon, UploadCloudIcon } from "lucide-react";
+import {
+  FilePenIcon,
+  PencilIcon,
+  PlusIcon,
+  Trash2Icon,
+  UploadCloudIcon,
+  XIcon,
+} from "lucide-react";
 import { useEffect, useState } from "react";
 import { dummyResumeData } from "../assets/dummyData";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+
   const colors = ["#9333ea", "#d97706", "#dc2626", "#0284c7", "#16a34a"];
 
   const [allResumes, setallResumes] = useState([]);
+  const [showUploadResume, setShowUploadResume] = useState(false);
+  const [showCreateResume, setShowCreateResume] = useState(false);
+  const [title, setTitle] = useState("");
+  const [resume, setResume] = useState(null);
+  const [editResumeId, setEditResumeId] = useState("");
+
   const loadAllResumes = async () => {
     setallResumes(dummyResumeData);
+  };
+
+  const createResume = (e) => {
+    e.preventDefault();
+    setShowCreateResume(false);
+    navigate(`/app/builder/res123`);
   };
 
   useEffect(() => {
@@ -22,7 +44,9 @@ const Dashboard = () => {
         </p>
 
         <div className="flex gap-4">
-          <button className="w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-green-500 hover:shadow-md transition-all duration-300">
+          <button
+            onClick={() => setShowCreateResume(true)}
+            className="w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-green-500 hover:shadow-md transition-all duration-300">
             <PlusIcon className="size-11 transition-all duration-300 p-2.5 bg-linear-to-r from-indigo-600 to-indigo-400 text-white rounded-full" />
             <p className="text-sm group-hover:text-green-600 transition-all duration-300">
               {" "}
@@ -68,14 +92,48 @@ const Dashboard = () => {
                 <p
                   className="absolute bottom-1 text-[11px] text-slate-400 group-hover:text-slate-500 transition-all duration-300 px-2 text-center"
                   style={{
-                    color: baseColor + "40",
+                    color: baseColor + "90",
                   }}>
                   Updated on {new Date(resume.updatedAt).toLocaleDateString()}
                 </p>
+
+                <div className="absolute top-1 right-1 group-hover:flex items-center hidden">
+                  <Trash2Icon className="size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors" />
+                  <PencilIcon className="size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors" />
+                </div>
               </button>
             );
           })}
         </div>
+
+        {showCreateResume && (
+          <form
+            onSubmit={createResume}
+            onClick={() => setShowCreateResume(false)}
+            className="fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-10 flex items-center justify-center">
+            <div
+              onClick={(e) => e.stopPropagation()}
+              className="relative bg-slate-50 border shadow-md rounded-lg w-full max-w-sm p-6">
+              <h2 className="text-xl font-bold mb-4">Create Resume</h2>
+              <input
+                type="text"
+                placeholder="Enter resume title"
+                required
+                className="w-full px-4 py-2 mb-4 focus:border-green-600 ring-green-600 rounded-lg"
+              />
+              <button className="w-full py-2 bg-green-600 text-white rounded hover:bg-green-700 transition-colors">
+                Create Resume
+              </button>
+              <XIcon
+                className="absolute top-4 right-4 text-slate-400 hover:text-slate-600 cursor-pointer transition-colors"
+                onClick={() => {
+                  setShowCreateResume(false);
+                  setTitle("");
+                }}
+              />
+            </div>
+          </form>
+        )}
       </div>
     </div>
   );
